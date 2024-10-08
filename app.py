e5652a4a80a3cbb66a05e85ea3814ac80bfe3554
ash <w1021968071@gmail.com>
2024-10-08 14:40:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,17 @@ def predict():
     final = np.array(int_features)
     data_unseen = pd.DataFrame([final], columns = cols)
     prediction = predict_model(model, data=data_unseen, round = 0)
-    prediction = int(prediction.Label[0])
-    return render_template('home.html', pred='Expected Bill will be {}'.format(prediction))
+    prediction = int(prediction.prediction_label[0])
+    return render_template('home.html', pred='Expected Bill will be {} 元'.format(prediction))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
     prediction = predict_model(model, data=data_unseen)
-    output = prediction.Label[0]
+    output = prediction.prediction_label[0]
     return jsonify(output)
+    # return prediction
 
 if __name__ == '__main__':
     app.run(debug=True)
